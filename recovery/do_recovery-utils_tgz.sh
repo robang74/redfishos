7da946a4f77ec72cb6ec68ae8d09a81845fb504f
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-21 15:24:18 +0200

    recovery: scripts to create recovery and sysdebug packages
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- recovery/do_recovery-utils_tgz.sh
+++ recovery/do_recovery-utils_tgz.sh
@@ -2,40 +2,46 @@
 
 url="https://vault.centos.org/centos/8/BaseOS/aarch64/os/Packages/"
 
+# binutils-2.30-108.el8_5.1.aarch64.rpm
+
 rpm_list="
-binutils-2.30-108.el8_5.1.aarch64.rpm
 libattr-2.4.48-3.el8.aarch64.rpm
 patch-2.7.6-11.el8.aarch64.rpm
 rsync-3.1.3-12.el8.aarch64.rpm
 pigz-2.4-4.el8.aarch64.rpm
 "
 
+tar_dir="etc bin usr var"
 name="recovery-utils"
 tgz="$name.tar.gz"
 dir="$name.dir"
 
 set -e
 
-mkdir -p $dir && cd $dir
+mkdir -p $dir && cd $dir && sudo rm -rf $tar_dir
 for i in $rpm_list; do wget -c $url/$i; done
-for i in *.rpm; do rpm2cpio $(basename $i) | sudo cpio -idmu -R root.root; done
+for i in $rpm_list; do 
+	rpm2cpio $(basename $i) | sudo cpio -idmu -R root.root
+done; echo
 
 usr_bin="
 addr2line ar as c++filt dwp elfedit gprof ld.bfd size
-ld.gold  nm objcopy objdump pigz ranlib readelf strip
+ld.gold  nm objcopy objdump ranlib readelf strip
 "
-usr_lib64="libbfd libopcodes"
+usr_lib64="libopcodes"
 
 #for i in $usr_bin $usr_lib64; do sudo rm -rf usr/bin/$i usr/lib64/$i*; done
 #rm -rf usr/lib/ usr/share/ #*.rpm
 
 #sudo chown -R root.root *
-set -x
+#set -x
+
+cd usr/bin; sudo ln -sf unpigz pigz; cd - >/dev/null
 sudo tar cvzf ../$tgz --exclude="usr/lib" --exclude="usr/share" \
-	$(for i in $usr_bin $usr_lib64; do echo --exclude=usr/bin/$i  \
-		--exclude=usr/lib64/$i*; done) etc usr
+	$(for i in $usr_bin $usr_lib64; do echo --exclude=usr/bin/$i \
+		--exclude=usr/lib64/$i*; done) $(ls -1d $tar_dir 2>/dev/null)
 cd ..
 #sudo rm -rf $dir
 sudo chown -R $USER.$USER $tgz
-du -ks $tgz
+echo; du -ks $tgz | tr '\t' ' '
 
--- recovery/do_sysdebug-utils_tgz.sh
+++ recovery/do_sysdebug-utils_tgz.sh
@@ -2,9 +2,10 @@
 
 url="https://vault.centos.org/centos/8/BaseOS/aarch64/os/Packages/"
 
-rpm_list="
+rpm_list_1="
 strace-5.7-3.el8.aarch64.rpm
 traceroute-2.1.0-6.el8.aarch64.rpm
+libunistring-0.9.9-3.el8.aarch64.rpm
 elfutils-libelf-0.185-1.el8.aarch64.rpm
 keyutils-libs-1.5.10-9.el8.aarch64.rpm
 elfutils-libs-0.185-1.el8.aarch64.rpm
@@ -13,18 +14,20 @@ json-c-0.13.1-2.el8.aarch64.rpm
 libidn2-2.2.0-1.el8.aarch64.rpm
 "
 
+tar_dir="etc bin usr var"
 name="sysdebug-utils"
 tgz="$name.tar.gz"
 dir="$name.dir"
 
 set -e
 
-mkdir -p $dir && cd $dir
-for i in $rpm_list; do 	wget -c $url/$i; done
+mkdir -p $dir && cd $dir && sudo rm -rf $tar_dir
+for i in $rpm_list_1; do wget -c $url/$i; done
 
 url="https://vault.centos.org/centos/8/AppStream/aarch64/os/Packages/"
 
-rpm_list="
+
+rpm_list_2="
 tcpdump-4.9.3-2.el8.aarch64.rpm
 nmap-ncat-7.70-6.el8.aarch64.rpm
 bind-utils-9.11.26-6.el8.aarch64.rpm
@@ -35,22 +38,26 @@ protobuf-c-1.3.0-6.el8.aarch64.rpm
 fstrm-0.6.1-2.el8.aarch64.rpm
 "
 
-for i in $rpm_list; do wget -c $url/$i; done
-for i in *.rpm; do rpm2cpio $(basename $i) | sudo cpio -idmu -R root.root; done
+for i in $rpm_list_2; do wget -c $url/$i; done
+for i in $rpm_list_1 $rpm_list_2; do
+	rpm2cpio $(basename $i) | sudo cpio -idmu -R root.root
+done; echo
 
 usr_bin="
-nsupdate delv arpaname mmdblookup dnstap-read
-genrandom named-checkzone nsec3hash isc-hmac-fixup
+nsupdate arpaname mmdblookup dnstap-read named-compilezone ddns-confgen
+genrandom named-checkzone nsec3hash isc-hmac-fixup delv tsig-keygen
 "
-for i in $usr_bin; do sudo rm -f usr/*bin/$i; done
+
+#for i in $usr_bin; do sudo rm -f usr/*bin/$i; done
 #rm -rf usr/lib/ usr/share/ usr/sbin/dns* #*.rpm
 
 #sudo chown -R root.root *
 sudo tar cvzf ../$tgz --exclude="usr/lib" --exclude="usr/share" \
-	$(for i in $usr_bin; do echo --exclude=usr/*bin/$i; done) \
-	--exclude="usr/sbin/dns*" etc usr var bin
+	$(for i in $usr_bin; do
+		echo --exclude=usr/*bin/$i --exclude=*bin/$i
+	done) --exclude="usr/sbin/dns*" $(ls -1d $tar_dir 2>/dev/null)
 cd ..
 #sudo rm -rf $dir
 sudo chown -R $USER.$USER $tgz
-du -ks $tgz
+echo; du -ks $tgz | tr '\t' ' '
 
