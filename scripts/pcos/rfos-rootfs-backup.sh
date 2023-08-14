2aa1ffcbc8248b41bea5dd8fac623f026eeb4583
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-08-14 18:33:50 +0200

    pcos: rfos-rootfs-backup.sh, check mobile data + aesthetics
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- scripts/pcos/rfos-rootfs-backup.sh
+++ scripts/pcos/rfos-rootfs-backup.sh
@@ -31,44 +31,86 @@ if [ "x${1:-}" = "x-v" ]; then
     v="v"; shift
 fi
 
-date_time=$(date +"%F-%H-%M-%S")
+#date_time=$(date +"%F-%H-%M-%S")
 date_time=$(date +"%Y%m%d%H%M%S")
+tarball="backup-rootfs-${date_time}.tar.gz"
 find_opts="-xdev ! -type d" # likely -print0 but tar has not --null
 tar_opts="--numeric-owner -p"
-excl_list="
-/root /tmp /var/tmp /usr/tmp /var/cache
-
-/usr/lib/locale /vendor /home /apex /odm /boot
-
+excl_list_0="/tmp /var/tmp /usr/tmp /var/cache /home /root"
+excl_list_1="/usr/lib/locale /vendor /apex /odm /boot"
+excl_list_2="/usr/libexec/droid-hybris"
+excl_list_3="/usr/share/themes /usr/share/man /usr/share/sailfish-tutorial
 /usr/share/locale /usr/share/ambience /usr/share/fonts /usr/share/translations
-/usr/share/sounds /usr/share/sailfish-tutorial /usr/share/licenses
-/usr/share/themes /usr/share/man
+/usr/share/sounds /usr/share/licenses"
 
-/usr/libexec/droid-hybris
-"
+case "${1:-}" in
+    "-1")
+        excl_list_strn="0"
+        excl_list=${excl_list_0}
+        ;;
+    "-2")
+        excl_list_strn="0 1"
+        excl_list="${excl_list_0} ${excl_list_1}"
+        ;;
+    "-3")
+        excl_list_strn="0 1 2"
+        excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2}"
+        ;;
+       *)
+        excl_list_strn="0 1 2 3"
+        excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2} ${excl_list_3}"
+        ;;
+esac
+
 for i in $excl_list; do find_opts="$find_opts ! -path $i/\*"; done
-
-tarball="backup-rootfs-${date_time}.tar.gz"
 
 # MAIN CODE EXECUTION ##########################################################
 
-afish getip
-sshcmd="ssh root@$sfos_ipaddr"
+mob_data_check_cmd="
+    { ifconfig rmnet_data1; ifconfig rmnet_data2; } | grep 'inet6* addr'
+"
+
+while true; do
+    afish getip
+    sshcmd="ssh root@$sfos_ipaddr"
+    echo
+    printf "=> Check mobile data connection on device..."
+    if $sshcmd "$mob_data_check_cmd" | grep -q 'inet6* addr'; then
+        echo " active"
+        echo
+        echo "WARNING: please, deactive mobile data on the samrtphone"
+        echo "         back-up should be use the USB connection, only"
+        echo
+        echo "         Press ENTER to retry or CTRL-C to stop"
+        sleep 1
+        read
+    else
+        echo " ok"
+        break
+    fi
+done
 
 echo
-echo "Creating $tarball by SSH/cat in one minute..."
+echo "=> Creating backup by SSH/cat in one minute..."
+echo "  \_ archive: $tarball"
+echo "  \_ exclusions lists: $excl_list_strn"
 
-time $sshcmd "find / $find_opts | tar ${v}c $tar_opts -T - | pigz -4Ric" |\
-    dd bs=1M iflag=fullblock of=$tarball 2>&1 |\
-        sed -ne "s,\(.* copied\),\n\\1,p"
+{
+    time $sshcmd \
+        "find / $find_opts | tar ${v}c $tar_opts -T - | pigz -4Ric" |\
+            dd bs=1M iflag=fullblock of=$tarball
+} 2>&1 | grep -v "tar: removing leading" | grep -E "real|copied" | tr '\t' ' '\
+    | sed -e "s/.* bytes (\(.*\), .*)\(.*\)/transfer speed: \\1\\2/" -e \
+        "s/real /execution time: /" | sed -e "s,^,  |  ,"
+echo "  \_ creation: completed"
 
 echo
-printf "Syncing archive file to local storage..."
+printf "=> Syncing archive file to local storage..."
 sync $tarball
 echo " OK"
 
 echo
-printf "Checking archive file for integrity..."
+printf "=> Checking archive file for integrity..."
 if tar tzf $tarball >/dev/null; then
     echo " OK"
     echo
