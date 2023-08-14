fdf0cc9a2d4810bb3eebc68e7e104068f4373931
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-08-14 21:44:15 +0200

    pcos: rfos-rootfs-backup.sh, review and rework
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- scripts/pcos/rfos-rootfs-backup.sh
+++ scripts/pcos/rfos-rootfs-backup.sh
@@ -18,7 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 ################################################################################
-# release: 0.0.1
+# release: 0.0.2
 
 set -eu
 
@@ -26,14 +26,8 @@ src_file_env "sfos-ssh-connect"
 
 # VARIABLES DEFINITIONS ########################################################
 
-v=""
-if [ "x${1:-}" = "x-v" ]; then
-    v="v"; shift
-fi
-
 #date_time=$(date +"%F-%H-%M-%S")
 date_time=$(date +"%Y%m%d%H%M%S")
-tarball="backup-rootfs-${date_time}.tar.gz"
 find_opts="-xdev ! -type d" # likely -print0 but tar has not --null
 tar_opts="--numeric-owner -p"
 excl_list_0="/tmp /var/tmp /usr/tmp /var/cache /home /root"
@@ -43,26 +37,48 @@ excl_list_3="/usr/share/themes /usr/share/man /usr/share/sailfish-tutorial
 /usr/share/locale /usr/share/ambience /usr/share/fonts /usr/share/translations
 /usr/share/sounds /usr/share/licenses"
 
-case "${1:-}" in
-    "-1")
-        excl_list_strn="0"
-        excl_list=${excl_list_0}
-        ;;
-    "-2")
-        excl_list_strn="0 1"
-        excl_list="${excl_list_0} ${excl_list_1}"
-        ;;
-    "-3")
-        excl_list_strn="0 1 2"
-        excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2}"
-        ;;
-       *)
-        excl_list_strn="0 1 2 3"
-        excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2} ${excl_list_3}"
-        ;;
-esac
+usage() {
+    echo
+    echo "USAGE: $(basename $0) [ -v | -h ] [ -1 | -2 | -3 ] "
+    echo
+    exit 0
+}
+
+while [ -n "${1:-}" ]; do
+    case $1 in
+        -h|--help)
+            usage
+            ;;
+        -1)
+            excl_list_strn="0"
+            excl_list=${excl_list_0}
+            ;;
+        -2)
+            excl_list_strn="0 1"
+            excl_list="${excl_list_0} ${excl_list_1}"
+            ;;
+        -3)
+            excl_list_strn="0 1 2"
+            excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2}"
+            ;;
+        -v)
+            v="v"
+            ;;
+         *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+if [ ! -n "$excl_list_strn" ]; then
+    excl_list_strn="0 1 2 3"
+    excl_list="${excl_list_0} ${excl_list_1} ${excl_list_2} ${excl_list_3}"
+fi
 
 for i in $excl_list; do find_opts="$find_opts ! -path $i/\*"; done
+
+tarball="backup-rootfs-${date_time}.tar.gz"
 
 # MAIN CODE EXECUTION ##########################################################
 
@@ -97,7 +113,7 @@ echo "  \_ exclusions lists: $excl_list_strn"
 
 {
     time $sshcmd \
-        "find / $find_opts | tar ${v}c $tar_opts -T - | pigz -4Ric" |\
+        "find / $find_opts | tar ${v:-}c $tar_opts -T - | pigz -4Ric" |\
             dd bs=1M iflag=fullblock of=$tarball
 } 2>&1 | grep -v "tar: removing leading" | grep -E "real|copied" | tr '\t' ' '\
     | sed -e "s/.* bytes (\(.*\), .*)\(.*\)/transfer speed: \\1\\2/" -e \
