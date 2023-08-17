6ce723ab97b06db294fe9e2b5b160721b5e2e7fe
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-08-17 16:15:25 +0200

    spacetabs.sh works also on recovery/ramdisk/init* not .orig
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- spacetabs.sh
+++ spacetabs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+initscripts=$(ls -1 recovery/ramdisk/init* | grep -Ev .orig ||:)
+
 for i in {1..80}; do
-    sed -i "s/ $//g" "$@" scripts/*.{sh,env} scripts/*os/*.{sh,env} || break
+    sed -i "s/ $//g" "$@" scripts/*.{sh,env} scripts/*os/*.{sh,env} \
+        $initscripts || break
 done
 
 for i in {1..80}; do
-    sed -i "s/\t/    /g" "$@" scripts/*.{sh,env} scripts/*os/*.{sh,env} || break
+    sed -i "s/\t/    /g" "$@" scripts/*.{sh,env} scripts/*os/*.{sh,env} \
+        $initscripts || break
 done
